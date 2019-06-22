dcee8f032e0494d141ab37dc922039c94d5cac3f
hongntt1906 <52079508+hongntt1906@users.noreply.github.com>
2019-06-22 18:25:00 +0700

    Update Form2.cs

--- WindowsFormsApplication1_ViDuTinhTong/WindowsFormsApplication1_ViDuTinhTong/Form2.cs
+++ WindowsFormsApplication1_ViDuTinhTong/WindowsFormsApplication1_ViDuTinhTong/Form2.cs
@@ -17,44 +17,66 @@ namespace WindowsFormsApplication1_ViDuTinhTong
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+       private void button1_Click(object sender, EventArgs e)
         {
-            string so = textBox1.Text;
-            int chu = int.Parse(so);
-            switch (chu)
-            { 
-                case 0:
-                    textBox2.Text = "Không";
-                    break;
-                case 1:
-                    textBox2.Text = "Một";
-                    break;
-                case 2:
-                    textBox2.Text = "Hai";
-                    break;
-                case 3:
-                    textBox2.Text = "Ba";
-                    break;
-                case 4:
-                    textBox2.Text = "Bốn";
-                    break;
-                case 5:
-                    textBox2.Text = "Năm";
-                    break;
-                case 6:
-                    textBox2.Text = "Sáu";
-                    break;
-                case 7:
-                    textBox2.Text = "Bảy";
-                    break;
-                case 8:
-                    textBox2.Text = "Tám";
-                    break;
-                case 9:
-                    textBox2.Text = "Chín";
-                    break;
+            string chu = textBox1.Text;
+            int so = int.Parse(chu);
 
+            string[] abc= new string[] {"không","một","hai","ba","bốn","năm","sáu","bảy","tám","chín"};
+
+            if (so / 10 == 0)
+                textBox2.Text = abc[so];
+            else if (so / 100 == 0)
+                textBox2.Text = chuc(so, abc);
+            else if (so / 1000 == 0)
+                textBox2.Text = tram(so, abc);
+            else if (so / 10000 == 0)
+                textBox2.Text = abc[so / 1000] + " nghìn " + tram(so % 1000, abc);
+            else if (so / 100000 == 0)
+                textBox2.Text = chuc(so / 1000, abc) + " nghìn " + tram(so % 1000, abc);
+            else if (so / 1000000 == 0)
+                textBox2.Text = tramnghin(so, abc);
+            else if (so / 10000000 == 0)
+                textBox2.Text = abc[so / 1000000] + " triệu " + tramnghin(so % 1000000, abc);
+            else if (so / 100000000 == 0)
+                textBox2.Text = chuc(so / 1000000, abc) + " triệu " + tramnghin(so % 1000000, abc);
+            else if (so / 1000000000 == 0)
+                textBox2.Text = tram(so / 1000000, abc) + " triệu " + tramnghin(so % 1000000, abc);
+            else textBox2.Text = "!!!!";
+        }
+
+        private string chuc(int c, string[] abc)
+        {
+            if (c == 0)
+                return " ";
+            if (c / 10 == 0)
+            { 
+                return " linh " + abc[c%10];
             }
+            else if (c / 10 == 1)
+            {
+                if (c % 10 == 0)
+                    return "mười";
+                else
+                    return " mười " + abc[c % 10];
+            }
+            else
+            {
+                if (c % 10 == 0)
+                    return abc[c / 10] + " mươi ";
+                else
+                    return abc[c / 10] + " mươi " + abc[c % 10];
+            }
+        }
+        private string tram(int t, string[] abc)
+        {
+            if (t == 0)
+                return " ";
+            return abc[t / 100] + " trăm " + chuc(t % 100, abc);
+        }
+        private string tramnghin(int tn, string[] abc)
+        {
+            return tram(tn / 1000, abc) + " nghìn " + tram(tn % 1000, abc);
         }
 
         private void button2_Click(object sender, EventArgs e)
